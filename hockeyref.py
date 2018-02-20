e3411c5921e0a2a49ee09c29aca954260085aac0
Matthew Devaney <34290969+yourekittenme@users.noreply.github.com>
2018-02-19 20:17:20 -0600

    Add files via upload

--- hockeyref.py
+++ hockeyref.py
@@ -1,5 +1,6 @@
 # import the required Python libraries
 from bs4 import BeautifulSoup
+from bs4 import Comment
 import datetime
 import pandas as pd
 import os
@@ -14,7 +15,6 @@ class HockeyReference:
         # initialize object variables
         self.df = None
         self.title = None
-        self.url = None
 
     def daily_activity_skaters(self, date_range_start, date_range_end):
 
@@ -33,11 +33,11 @@ class HockeyReference:
             stats_month = date_extract.strftime('%m')
             stats_day = date_extract.strftime('%d')
             stats_year = date_extract.strftime('%Y')
-            self.url = 'https://www.hockey-reference.com/friv/dailyleaders.fcgi?month={}&day={}&year={}'.format(
+            stats_webpage = 'https://www.hockey-reference.com/friv/dailyleaders.fcgi?month={}&day={}&year={}'.format(
                              stats_month, stats_day, stats_year)
 
             # create the soup object
-            r = requests.get(self.url)
+            r = requests.get(stats_webpage)
             html_doc = r.text
             soup = BeautifulSoup(html_doc, 'html.parser')
 
@@ -59,7 +59,10 @@ class HockeyReference:
                         pass
 
                 # load the skaters data into a Pandas DataFrame
-                columns_list = ['Player Name','Position','Team','Home Away','Opponent','Result','Boxscore','G','A','PTS','Plus Minus','PIM','EV','PP','SH','GW','EV','PP','SH','S','S%','SHFT','TOI','HIT','BLK','FOW','FOL','FO%']
+                columns_list = ['Player Name','Position','Team','HomeAway','Opponent','Result','Boxscore',
+                                'G','A','PTS','PlusMinus','PIM','EVG','PPG','SHG','GWG','EVA','PPA','SHA','S','SPER',
+                                'SHFT','TOI','HIT','BLK','FOW','FOL','FOPER']
+
                 df_skaters = pd.DataFrame.from_records(skaters_records, columns=columns_list)
                 df_skaters.insert(0, 'Date', date_extract)
 
@@ -67,9 +70,9 @@ class HockeyReference:
                 df_player_id = pd.DataFrame(player_id_list, columns=['Player ID'])
                 df_concat = pd.concat([df_player_id, df_skaters], axis=1)
 
-                # delete the Boxscore column and replace the contents of the Home Away column
+                # delete the Boxscore column and replace the contents of the HomeAway column
                 del df_concat['Boxscore']
-                df_concat['Home Away'] = df_concat['Home Away'].apply(lambda x: 'Home' if x == '' else 'Away')
+                df_concat['HomeAway'] = df_concat['HomeAway'].apply(lambda x: 'Home' if x == '' else 'Away')
 
                 # index the skaters DataFrame by Date and Player ID and sort by the index
                 df_concat.set_index(['Date','Player ID'],inplace=True, drop=True)
@@ -103,11 +106,11 @@ class HockeyReference:
             stats_month = date_extract.strftime('%m')
             stats_day = date_extract.strftime('%d')
             stats_year = date_extract.strftime('%Y')
-            self.url = 'https://www.hockey-reference.com/friv/dailyleaders.fcgi?month={}&day={}&year={}'.format(
+            stats_webpage = 'https://www.hockey-reference.com/friv/dailyleaders.fcgi?month={}&day={}&year={}'.format(
                 stats_month, stats_day, stats_year)
 
             # create the soup object
-            r = requests.get(self.url)
+            r = requests.get(stats_webpage)
             html_doc = r.text
             soup = BeautifulSoup(html_doc, 'html.parser')
 
@@ -129,8 +132,8 @@ class HockeyReference:
                         pass
 
                 # load the skaters data into a Pandas DataFrame
-                columns_list = ['Player', 'Position','Team', 'Home Away', 'Opponent', 'Result', 'Boxscore', 'DEC',
-                                'GA', 'SA', 'SV', 'SV%', 'SO', 'PIM', 'TOI']
+                columns_list = ['Player', 'Position','Team', 'HomeAway', 'Opponent', 'Result', 'Boxscore', 'DEC',
+                                'GA', 'SA', 'SV', 'SVPER', 'SO', 'PIM', 'TOI']
                 df_goalies = pd.DataFrame.from_records(goalies_records, columns=columns_list)
                 df_goalies.insert(0, 'Date', date_extract)
 
@@ -138,9 +141,9 @@ class HockeyReference:
                 df_player_id = pd.DataFrame(player_id_list, columns=['Player ID'])
                 df_concat = pd.concat([df_player_id, df_goalies], axis=1)
 
-                # delete the Boxscore column and replace the contents of the Home Away column
+                # delete the Boxscore column and replace the contents of the HomeAway column
                 del df_concat['Boxscore']
-                df_concat['Home Away'] = df_concat['Home Away'].apply(lambda x: 'Home' if x == '' else 'Away')
+                df_concat['HomeAway'] = df_concat['HomeAway'].apply(lambda x: 'Home' if x == '' else 'Away')
 
                 # index the skaters DataFrame by Date and Player ID and sort by the index
                 df_concat.set_index(['Date', 'Player ID'], inplace=True, drop=True)
@@ -237,6 +240,57 @@ class HockeyReference:
 
             time.sleep(3)
 
+    def team_stats(self, date_year):
+
+        # give the object a title
+        self.title = 'Teams'
+
+        # generate the hockey reference url
+        stats_webpage = 'https://www.hockey-reference.com/leagues/NHL_{}.html'.format(date_year)
+
+        # create the soup object
+        r = requests.get(stats_webpage)
+        html_doc = r.text
+        soup = BeautifulSoup(html_doc, 'html.parser')
+
+        # extract the comment containing the teams table
+        team_table = soup.find(id='all_stats')
+        team_comment = team_table.find(string=lambda text: isinstance(text, Comment))
+
+        # convert the comment back into a string another soup object from it
+        str_comment = str(team_comment)
+        soup2 = BeautifulSoup(str_comment, 'html.parser')
+
+        # get all records from the teams table and delete the totals row
+        team_table2 = soup2.find(id='stats').find_all('td')
+        team_tbody = [cell.get_text() for cell in team_table2]
+        team_records = [tuple(team_tbody[row: row + 32]) for row in range(0, len(team_tbody), 32)]
+        del team_records[-1]
+
+        # make a list of team ids
+        team_table3 = soup2.find(id='stats').find('tbody').find_all('a')
+        list_team_ids = [a['href'][7:10] for a in team_table3]
+
+        # load the teams data into a Pandas DataFrame
+        columns_list = ['Team Name', 'Average Age', 'GP', 'W', 'L', 'OL', 'PTS', 'PTS%', 'GF', 'GA', 'SOW', 'SOL',
+                        'SRS', 'SOS', 'TGG', 'EVGF', 'EVGA', 'PP', 'PPO', 'PPPER', 'PPA', 'PPOA', 'PKPER', 'SH', 'SHA',
+                        'PIMG', 'oPIMG', 'S', 'SPER', 'SA', 'SVPER', 'PDO']
+        df_teams = pd.DataFrame.from_records(team_records, columns=columns_list)
+
+        # concatenate the team ids to the teams DataFrame
+        df_team_id = pd.DataFrame(list_team_ids, columns=['Team ID'])
+        df_concat = pd.concat([df_team_id, df_teams], axis=1)
+
+        # set the index to team ID
+        df_concat.set_index('Team ID', drop=True, inplace=True)
+        
+        # delete the average age column
+        del df_concat['Average Age']
+
+        self.df = df_concat
+
+        time.sleep(3)
+
     def output_csv(self, dir_csv):
 
         # create a new directory if it does not already exist
@@ -249,24 +303,28 @@ class HockeyReference:
         self.df.to_csv(path_csv)
 
 
-# directory of the output files
-dir_csv = os.path.join(os.getcwd(), 'csv')
+if __name__ == '__main__':
 
-# define the range of dates to extract
-date_range_start = datetime.datetime(2017, 10, 5)
-date_range_end = datetime.datetime(2017, 10, 6)
-date_year = 2018
+    # directory of the output files
+    dir_csv = os.path.join(os.getcwd(), 'csv')
 
-r = HockeyReference()
-r.team_rosters(date_year)
-r.output_csv(dir_csv)
+    # define the range of dates to extract
+    date_range_start = datetime.datetime(2018, 2, 17)
+    date_range_end = datetime.datetime(2018, 2, 17)
+    date_year = 2018
 
-"""
-s = HockeyReference()
-s.daily_activity_skaters(date_range_start, date_range_end)
-s.output_csv(dir_csv)
+    t = HockeyReference()
+    t.team_stats(date_year)
+    t.output_csv(dir_csv)
 
-g = HockeyReference()
-g.daily_activity_goalies(date_range_start, date_range_end)
-g.output_csv(dir_csv)
-"""
+    r = HockeyReference()
+    r.team_rosters(date_year)
+    r.output_csv(dir_csv)
+
+    s = HockeyReference()
+    s.daily_activity_skaters(date_range_start, date_range_end)
+    s.output_csv(dir_csv)
+
+    g = HockeyReference()
+    g.daily_activity_goalies(date_range_start, date_range_end)
+    g.output_csv(dir_csv)
